da1deff6d70c21856bd11a8d5fe1762b68feb128
cabbage_cultist <60555764+cabbagecultist@users.noreply.github.com>
2024-10-06 17:54:27 -0400

    ACTUALLY fix the site

--- animate.js
+++ animate.js
@@ -1,4 +1,4 @@
-import * as THREE from "three";
+import * as THREE from "./node_modules/three/build/three.module.js";
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 const scaleX = 0.75
