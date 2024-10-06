1cc2cdad6e820f19ce4695c810c5a3761128bc69
Jazzbank <hasibhossain2006@gmail.com>
2024-10-06 15:43:55 -0400

    ergefgs

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ var i;
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function() {
       this.classList.toggle("active");
-      var content = this.nextElementSibling;
+      var content = document.getElementById('content');
       if (content.style.maxHeight){
         content.style.maxHeight = null;
+        content.style.border = 'none';
+        content.style.padding = '0px'
       } else {
         content.style.maxHeight = content.scrollHeight + "px";
+        content.style.border = '2px white solid'
+        content.style.padding = '5px'
       } 
     });
   }
